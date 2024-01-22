84a6b170085c30a38907f9cac9657458598085a4
Igor <igorbritto97@hotmail.com>
2024-01-22 06:47:42 -0500

    desenvolvimento simulador -> ajustes e correções no fluxo da simulação

--- src/main/java/br/com/simulatorAppServer/models/SimulationModel.java
+++ src/main/java/br/com/simulatorAppServer/models/SimulationModel.java
@@ -1,97 +1,125 @@
 package br.com.simulatorAppServer.models;
 
 import br.com.simulatorAppServer.enums.ServerlessExecutionEnum;
-import br.com.simulatorAppServer.utils.ServerlessFunctionSortingComparator;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import static java.lang.String.format;
+import static br.com.simulatorAppServer.enums.ServerlessExecutionEnum.*;
 
 @Getter
 @Setter
 @Slf4j
 public class SimulationModel {
+    private static final int MAX_RUNNING_INSTANCES = 10000;
+
     private LocalDateTime startTime;
     private LocalDateTime endTime;
     private SimulationInputDto input;
-    private List<ServerlessFunctionModel> runningInstances;
-    private List<ServerlessFunctionModel> idleInstances;
+
+    private List<ServerlessFunctionModel> instances;
+    private List<ServerlessFunctionModel> allRunningInstances;
+    private List<ServerlessFunctionModel> allInstanceTimes;//?
+    private List<ServerlessFunctionModel> allIdleInstances;
     private List<ServerlessFunctionModel> finishedInstances;
+    private List<Double> allColdArrivalTimes;
+    private List<Double> allWarmArrivalTimes;
+    private List<ServerlessFunctionModel> allRejectedInstances;
+    private Integer allInstancesCount;
 
     public SimulationModel(SimulationInputDto input) {
         this.input = input;
-        this.runningInstances = new ArrayList<>();
-        this.idleInstances = new ArrayList<>();
+        this.instances = new ArrayList<>();
+        this.allRunningInstances = new ArrayList<>();
+        this.allInstanceTimes = new ArrayList<>();
+        this.allIdleInstances = new ArrayList<>();
         this.finishedInstances = new ArrayList<>();
+        this.allColdArrivalTimes = new ArrayList<>();
+        this.allWarmArrivalTimes = new ArrayList<>();
+        this.allRejectedInstances = new ArrayList<>();
+        this.allInstancesCount = 0;
     }
 
-    public void addRunningInstance(Double time, ServerlessExecutionEnum executionType) {
-        ServerlessFunctionModel function = new ServerlessFunctionModel(time, executionType);
-        int idx = Collections.binarySearch(this.runningInstances, null, new ServerlessFunctionSortingComparator());
-        this.runningInstances.add(Math.max(idx, 0), function);
-
-        log.info(format("Add new instance! Execution time: %s - type: %s. Total number of runningInstances: %s", time, executionType.getDescription(), runningInstances.size()));
+    public boolean hasServers() {
+        return instances.size() > 0;
     }
 
-    public void addRejectedInstance(Double time) {
-        ServerlessFunctionModel function = new ServerlessFunctionModel(time, true);
-        this.finishedInstances.add(function);
+    public void coldStartArrival(double arrivalTime, double executionTime, long threshold)  {
+        if(getRunningInstances().size() == MAX_RUNNING_INSTANCES) {
+            log.info("REJECTING INSTANCE........");
+            ServerlessFunctionModel rejected = new ServerlessFunctionModel(arrivalTime, executionTime, threshold);
+            this.finishedInstances.add(rejected);
+            this.allRejectedInstances.add(rejected);
+        } else {
+            log.info("COLD START BABY - {} ", executionTime);
+            this.allInstancesCount++;
+            ServerlessFunctionModel function = new ServerlessFunctionModel(arrivalTime, executionTime, threshold, COLD);
+            this.instances.add(function);
+            this.allRunningInstances.add(function);
+            this.allColdArrivalTimes.add(arrivalTime);
+        }
     }
 
-    public void addIdleInstance() {
-        ServerlessFunctionModel function = new ServerlessFunctionModel(input.getThreshold());
-        this.idleInstances.add(this.idleInstances.size(), function);
-        log.info(format("Add idle instance! Threshold: %s. Total number of idleInstances: %s", input.getThreshold(), idleInstances.size()));
+    public void warmStartArrival(double arrivalTime, double executionTime, long threshold) {
+        log.info("WAAAAAARM START HERE LETGO - {} ", executionTime);
+        this.allInstancesCount++;
+        ServerlessFunctionModel function = this.instances.stream().filter(item -> item.getExecutionType().equals(IDLE)).findFirst().orElseThrow(() -> new RuntimeException("ERROR - Idle instance not found"));
+        function.startWarmExecution(arrivalTime, executionTime, threshold);
+        this.instances.add(function);
+        this.allRunningInstances.add(function);
+        this.allWarmArrivalTimes.add(arrivalTime);
     }
 
-    public void removeIdle() {
-        ServerlessFunctionModel function = this.idleInstances.remove(0);
-        log.info(format("Removing IDLE function: TYPE: %s. EXEC TIME: %s. CURRENT TIME: %s", function.getExecutionType().getDescription(), function.getExecutionTime(), function.getCurrentTime()));
-    }
-
-    public void updateInstancesCurrentTime(Double time) {
-        List<ServerlessFunctionModel> runningToRemove = new ArrayList<>();
-        for (ServerlessFunctionModel function : runningInstances) {
-            double newCurrentTime = function.getCurrentTime() - time;
-            function.setCurrentTime(newCurrentTime);
-            if (newCurrentTime <= 0.0) {
-                addIdleInstance();
-                finishedInstances.add(function);
-                runningToRemove.add(function);
+    public int getNextEventIndex() {
+        List<Double> nextEventList = this.instances.stream().map(ServerlessFunctionModel::getNextEventTime).collect(Collectors.toList());
+        int idx = 0;
+        double min = nextEventList.get(idx);
+        for(int i = 1; i < nextEventList.size(); i++) {
+            if(nextEventList.get(i) < min) {
+                idx = i;
             }
         }
-        if(!runningToRemove.isEmpty()) {
-            log.info(format("Removing running instance! Total number of instances to remove: %s", runningToRemove.size()));
-            runningInstances.removeAll(runningToRemove);
-        }
+        log.info("FOUND MIN NEXT EVENT VALUE: {} - IDX: {} ", nextEventList.get(idx), idx);
+        return idx;
 
-        List<ServerlessFunctionModel> idleToRemove = new ArrayList<>();
-        for (ServerlessFunctionModel function : idleInstances) {
-            double newCurrentTime = function.getCurrentTime() - time;
-            function.setCurrentTime(newCurrentTime);
-            if (newCurrentTime <= 0.0) {
-                finishedInstances.add(function);
-                idleToRemove.add(function);
-            }
-        }
+        //acho que essa parte aqui de cima esta com problema....... na execução ele mostra que o tempo minimo encontrado é um que ja passou.....
+//        this.instances.forEach(function -> {
+//
+//        });
+    }
 
-        if(!idleToRemove.isEmpty()){
-            log.info(format("Removing idle instance! Total number of instances to remove: %s", idleToRemove.size()));
-            idleInstances.removeAll(idleToRemove);
+    public void transitionEndingInstance(int idx) {
+        ServerlessExecutionEnum oldState = this.instances.get(idx).transitionToNextState();
+        if(oldState == IDLE) {
+            log.info("TRANSITIONING {} FROM IDLE TO FINISHED", idx);
+            this.finishedInstances.add(this.instances.get(idx));
+            this.instances.remove(idx);
         }
+        else {
+            log.info("TRANSITIONING {} TO IDLE FROM RUNNINGGGGGGGGG", idx);
+            this.instances.get(idx).setExecutionType(IDLE);
+            this.allIdleInstances.add(this.instances.get(idx));
+        }
+    }
+
+    public boolean isWarmAvailable() {
+        return this.getIdleInstances().size() > 0;
+    }
+
+    public List<ServerlessFunctionModel> getIdleInstances() {
+        return this.instances.stream().filter(instance -> instance.getExecutionType().getId().equals(ServerlessExecutionEnum.IDLE.getId())).collect(Collectors.toList());
+    }
+
+    public List<ServerlessFunctionModel> getRunningInstances() {
+        return this.instances.stream().filter(instance -> !instance.getExecutionType().getId().equals(ServerlessExecutionEnum.IDLE.getId())).collect(Collectors.toList());
     }
 
     public long getRejectedInstancesSize() {
         return finishedInstances.stream().filter(ServerlessFunctionModel::getRejected).count();
     }
 }
-/*
-    P
-
-*/
